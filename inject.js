dd8cd908b0cd169ae5ad926026456ccb63516f95
Alex Nichol <unixpickle@gmail.com>
2024-05-21 13:56:39 -0400

    fix

--- inject.js
+++ inject.js
@@ -1,14 +1,25 @@
-var docs = document.getElementsByClassName('islib');
-for (var i = 0; i < docs.length; i++) {
-    docs[i].click();
-    const url = new URL(docs[i].href);
-    const subURL = url.searchParams.get('imgurl');
-    const pathComps = subURL.split('/');
-    const fileName = pathComps[pathComps.length - 1];
-    const msg = {
-        filename: fileName,
-        url: subURL,
-    };
-    // TODO: see about removing the callback.
-    chrome.runtime.sendMessage(msg, function () { });
-}
+(async function () {
+    async function sleep() {
+        return new Promise((resolve, reject) => {
+            setTimeout(resolve, 10);
+        });
+    }
+
+    var docs = document.getElementsByClassName('islib');
+    for (var i = 0; i < docs.length; i++) {
+        docs[i].click();
+        if (!docs[i].href) {
+            await sleep();
+        }
+        const url = new URL(docs[i].href);
+        const subURL = url.searchParams.get('imgurl');
+        const pathComps = subURL.split('/');
+        const fileName = pathComps[pathComps.length - 1];
+        const msg = {
+            filename: fileName,
+            url: subURL,
+        };
+        // TODO: see about removing the callback.
+        chrome.runtime.sendMessage(msg, function () { });
+    }
+})();
